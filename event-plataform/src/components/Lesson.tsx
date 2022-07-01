3f7d835a945131cc374b40124bbbcde33203e698
rickydomingos <ricardodomingos97@gmail.com>
2022-07-01 16:14:20 -0300

    videoId access slug route

--- event-plataform/src/components/Lesson.tsx
+++ event-plataform/src/components/Lesson.tsx
@@ -2,6 +2,7 @@ import { CheckCircle, Lock } from "phosphor-react";
 
 import { isPast, format } from "date-fns";
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -19,7 +20,7 @@ export function Lesson(props: LessonProps) {
   );
 
   return (
-    <a href="#" className="group">
+    <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300">
         {availableDateFormatted}
       </span>
@@ -44,6 +45,6 @@ export function Lesson(props: LessonProps) {
 
         <strong className="text-gray-200 mt-5 block">{props.title}</strong>
       </div>
-    </a>
+    </Link>
   );
 }
