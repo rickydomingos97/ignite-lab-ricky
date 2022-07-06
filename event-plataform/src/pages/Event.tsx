4a33730a00aa2f53eba5573d834bb74aa11de141
rickydomingos <ricardodomingos97@gmail.com>
2022-07-06 15:33:51 -0300

    mutations done

--- event-plataform/src/pages/Event.tsx
+++ event-plataform/src/pages/Event.tsx
@@ -9,7 +9,7 @@ export function Event() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen w-full mx-auto">
       <Header />
-      <main className="flex flex-1">
+      <main className="w-h-full flex flex-1">
         {slug ? <Video lessonSlug={slug} /> : <div className="flex-1" />}
 
         <Sidebar />
