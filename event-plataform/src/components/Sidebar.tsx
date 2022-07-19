8e1097cedb8a7f1702470e2b9d271a2a0c66acb9
rickydomingos <ricardodomingos97@gmail.com>
2022-07-19 13:52:26 -0300

    pace loading pages

--- event-plataform/src/components/Sidebar.tsx
+++ event-plataform/src/components/Sidebar.tsx
@@ -7,7 +7,8 @@ export function Sidebar() {
   console.log(data);
 
   return (
-    <aside className="min-h-screen w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+    <aside className="md:min-h-screen md:w-[348px] 
+     bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
         Cronograma de aulas
       </span>
